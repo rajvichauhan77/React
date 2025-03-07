1405eeffd924dcf8235ab90104644d6d3d6d43c0
rajvichauhan77 <chauhanrajvi77@gmail.com>
2025-03-07 17:14:02 +0530

    srt

--- todo/src/components/List.jsx
+++ todo/src/components/List.jsx
@@ -3,16 +3,19 @@ import { Container, Row, Col, Card } from "react-bootstrap";
 
 
 
-export function List({list}){
+export function List({list, setList}){
 
-
-
-    function edit(){
-        
+    function updatetask(id){
+            const updatetask = list.map((ele) => ele.id === id ? {...ele, status : !ele.status} : ele)
+            setList(updatetask)
     }
 
+    function deltask(id){
+        const deltask = list.filter((ele) => ele.id != id)
+        setList(deltask)
+    }
 
-    console.log(list)
+    // console.log(list)
 
     return(
         <>
@@ -21,11 +24,11 @@ export function List({list}){
            <Row className="g-2 m-2 d-flex">
             {
                 list.map((ele) => (
-                    <Col  key={ele.key} className="w-100 col-lg-4">
+                    <Col  key={ele.key} className="col-lg-4">
                         <Card className="d-flex">
                              {ele.task}
-                             <button className="btn mt-2 m-auto btn-outline-secondary btn-sm w-50" onClick={edit}>Edit</button>
-                             <button  className="btn mt-1 m-auto btn-outline-secondary btn-sm w-50" onClick={del}>Delete</button>
+                             <button className="btn mt-2 m-auto btn-outline-secondary btn-sm w-50" onClick={() => updatetask(ele.id)}>Edit</button>
+                             <button  className="btn mt-1 m-auto btn-outline-secondary btn-sm w-50" onClick={() => deltask(ele.id)}>Delete</button>
                         </Card>
                        
                     </Col>
